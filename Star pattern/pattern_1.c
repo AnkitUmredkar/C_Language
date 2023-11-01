6ba9114a9ee8f6b2143b4dc8382062c39e91388a
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-01 19:10:29 +0530

    Add files via upload

---
+++ Star pattern/pattern_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	printf("  - - - - -\n");
+	printf(" | \t   |\n");
+	printf(" | \t   |\n");
+	printf(" |  Ankit  |\n");
+	printf(" | \t   |\n");
+	printf(" | \t   |\n");
+	printf("  - - - - -\n\n");
+}
---
+++ Star pattern/pattern_2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	printf("  - - - \n");
+	printf(" |     |\n");
+	printf(" A     |\n");
+	printf(" n     |\n");
+	printf(" k     |\n");
+	printf(" i     |\n");
+	printf(" t     |\n");
+	printf(" |     |\n");
+	printf("  - - - ");
+}
