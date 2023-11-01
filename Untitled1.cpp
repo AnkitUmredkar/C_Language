0c6d88060b8f7181afcfa23848a510be624b4712
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-01 19:36:56 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+	
+	printf("Enter the value of n : ");
+	scanf("%d",&n);
+	
+	if(n!=5)
+	{
+		printf("1");
+	}
+	else
+	{
+		printf("0");
+	}
+	
+	return 0;
+}
