478be7f194a9d85734460f618a65565c54e496b9
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-04 21:19:31 +0530

    Add files via upload

---
+++ Types_of_operator/relational_op.c
@@ -0,0 +1,23 @@
+// Example of relational operators
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 5, c = 10;
+
+    printf("\n%d == %d is %d \n", a, b, a == b);
+    printf("%d == %d is %d \n", a, c, a == c);
+    printf("%d > %d is %d \n", a, b, a > b);
+    printf("%d > %d is %d \n", a, c, a > c);
+    printf("%d < %d is %d \n", a, b, a < b);
+    printf("%d < %d is %d \n", a, c, a < c);
+    printf("%d != %d is %d \n", a, b, a != b);
+    printf("%d != %d is %d \n", a, c, a != c);
+    printf("%d >= %d is %d \n", a, b, a >= b);
+    printf("%d >= %d is %d \n", a, c, a >= c);
+    printf("%d <= %d is %d \n", a, b, a <= b);
+    printf("%d <= %d is %d \n", a, c, a <= c);
+
+    return 0;
+}
