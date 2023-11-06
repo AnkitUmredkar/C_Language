9d88c2451b59360d87fc910c58b368b982a825ea
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-06 17:29:43 +0530

    Add files via upload

---
+++ Bio_data.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	printf("My basic information : \n\n");
+	
+	printf("Name : Ankit Narendrabhai Umredkar\n");
+	printf("Age : 18\n");
+	printf("Birth Date : 11-12-2005\n");
+	printf("Gender : Male\n");
+	printf("Mobile number : 9328871876\n");
+	printf("Address : Plot no.2 Shivdarshan socitey, parvatgam,surat-395010");
+	printf("Hobby : reading,playing");	
+}
