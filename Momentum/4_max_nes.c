360a3b1c4e3b37730131eff9d939327a5555fe82
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-08 21:04:34 +0530

    Add files via upload

---
+++ Momentum/4_max_nes.c
@@ -0,0 +1,73 @@
+//3.Write C program to find out max from 4 numbers using nested if.
+
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c,d;
+	
+	printf("Enter the value of a : ");
+	scanf("%d",&a);
+	
+	printf("Enter the value of b : ");
+	scanf("%d",&b);
+	
+	printf("Enter the value of c : ");
+	scanf("%d",&c);
+	
+	printf("Enter the value of d: ");
+	scanf("%d",&d);
+	
+	if(a>b)
+	{
+		if(a>c)
+		{
+			if(a>d)
+			{
+				printf("a is max");
+			}
+			else
+			{
+				printf("d is max");
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				printf("c is max");
+			}
+			else
+			{
+				printf("d is max");
+			}
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			if(b>d)
+			{
+				printf("b is max");
+			}
+			else
+			{
+				printf("d is max");
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				printf("c is max");
+			}
+			else
+			{
+				printf("d is max");
+			}
+		}
+	}
+	
+	return 0;
+}
