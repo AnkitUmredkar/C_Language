16320d70932796287cccaf6331b50c95981c6a82
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-09 15:53:43 +0530

    Add files via upload

---
+++ while loop/leap_year.c
@@ -0,0 +1,16 @@
+//7. WAP to Find leap year from 2000 to 3000 using while loop.
+
+#include<stdio.h>
+
+int main()
+{
+	int x = 2000;
+	
+	while(x<=3000)
+	{
+		printf("%d ",x);
+		x+=4;
+	}
+	
+	return 0;
+}
