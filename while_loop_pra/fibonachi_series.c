33b1b1f6368d11c8d6a74046a13b6948f3275e4b
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-11 20:37:08 +0530

    Add files via upload

---
+++ while_loop_pra/fibonachi_series.c
@@ -0,0 +1,22 @@
+//print fibonachi series
+
+#include<stdio.h>
+
+int main()
+{
+	int first=0,second=1,add,x=0,n;
+	
+	printf("Enter the limit of series : ");
+	scanf("%d",&n);
+	
+	while(x<=n)
+	{
+		printf("%d ",first);
+		add = first + second;
+		first = second;
+		second = add;
+		x++;
+	}
+	
+	return 0;
+}
