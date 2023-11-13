f8e107e56477d8c320eb58a245800c91cf7c8a3e
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-13 18:24:21 +0530

    Add files via upload

---
+++ while_loop_pra/reverce_while.c
@@ -0,0 +1,18 @@
+//wap to find reverce number
+
+#include<stdio.h>
+
+void main()
+{
+	int n,r;
+	
+	printf("n : ");
+	scanf("%d",&n);
+	
+	while(n>0)
+	{
+		r = n%10;
+		printf("%d",r);
+		n = n/10;
+	}
+}
