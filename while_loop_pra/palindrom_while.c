73ec252a8be90257f3434e18618bfb201ae6c7a3
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-13 18:23:28 +0530

    Add files via upload

---
+++ while_loop_pra/palindrom_while.c
@@ -0,0 +1,29 @@
+//wap to find entered number is armstrong or not using while loop.
+
+#include<stdio.h>
+
+void main()
+{
+	int n,r,org,p=0;
+	
+	printf("n : ");
+	scanf("%d",&n);
+	
+	org = n;
+	
+	while(n>0)
+	{
+		r = n % 10;
+		p = r + (p*10);
+		n = n / 10;
+	}
+	
+	if(p == org)
+	{
+		printf("Yes, you entered palindrom number");
+	}
+	else
+	{
+		printf("You entered number is not palindrom");
+	}
+}
