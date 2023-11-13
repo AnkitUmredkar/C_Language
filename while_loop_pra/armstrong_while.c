a200565177baf7d505d683822246446df64310e2
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-13 18:22:59 +0530

    Add files via upload

---
+++ while_loop_pra/armstrong_while.c
@@ -0,0 +1,29 @@
+//wap to find entered number is armstrong or not using while loop.
+
+#include<stdio.h>
+
+void main()
+{
+	int n,r,org,arm=0;
+	
+	printf("n : ");
+	scanf("%d",&n);
+	
+	org = n;
+	
+	while(n>0)
+	{
+		r = n % 10;
+		arm = arm + (r*r*r);
+		n = n / 10;
+	}
+	
+	if(arm == org)
+	{
+		printf("Yes, you entered armstrong number");
+	}
+	else
+	{
+		printf("You entered number is not armstrong");
+	}
+}
