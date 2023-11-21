45195ef6e9720a811aa77b441ab0ef3cfefc4865
Ankit Umredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-21 18:14:52 +0530

    Add files via upload

---
+++ do-while loop/do_while_sum_1 to n.c
@@ -0,0 +1,21 @@
+//8. WAP to print sum 1 to N using do while loop.
+
+#include<stdio.h>
+
+int main()
+{
+	int n,x=1,sum=0;
+	
+	printf("Enter the value of n : ");
+	scanf("%d",&n);
+	
+	do
+	{
+		sum = sum +x;
+		x++;
+	}while(x<=n);
+	
+	printf("\nThe sum of 1 to %d is : %d",n,sum);
+	
+	return 0;
+}
