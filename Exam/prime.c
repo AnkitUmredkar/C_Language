b396644b4a991a6facd4ce93f81058c8c024dcfe
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-22 18:24:06 +0530

    Update prime.c

--- Exam/prime.c
+++ Exam/prime.c
@@ -9,5 +9,12 @@ int main()
 	printf("Enter the value of n : ");
 	scanf("%d",&n);
 	
-	printf("");
+	if(a%2!=0 && a%3!=0 && a%5!=0 && a%7!=0 && a%11!=0 ||(a==2 || a==3 || a==5 || a==7))
+	{
+		printf("prime");
+	}
+	else
+	{
+		printf("not prime");
+	}
 }
