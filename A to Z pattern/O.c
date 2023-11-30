a9a8e824f946ba54b0e07f1875d03d77968a0d8b
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-11-30 14:18:23 +0530

    Add files via upload

---
+++ A to Z pattern/O.c
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+
+int main()
+{	
+	int i,j;
+	
+	for(i=1; i<=7; i++)
+	{
+		for(j=1; j<=3; j++)
+		{
+			if(i==1 || i==7)
+			{
+				printf(" * ");
+			}
+			else if(j==1 || j==4)
+			{
+				printf("*       *");
+			}
+		}
+		printf("\n");
+	}
+	
+ 	return 0;
+}
