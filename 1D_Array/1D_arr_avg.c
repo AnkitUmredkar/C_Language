4d28cf96f3621c82e5ceccea5977e7bf0a8c6d1d
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-05 16:55:21 +0530

    Add files via upload

---
+++ 1D_Array/1D_arr_avg.c
@@ -0,0 +1,34 @@
+//3. WAP to find the average of the 1D array.
+
+#include<stdio.h>
+
+int main()
+{
+	int n,i;
+	float sum=0;
+	
+	printf("Enter the size of array : ");
+	scanf("%d",&n);
+	
+	int arr[n];
+	
+	printf("\nEnter the elements of array  \n");
+	
+	for(i=0; i<n; i++)
+	{
+		printf("Enter the value of arr[%d] : ",i);
+		scanf("%d",&arr[i]);
+		sum = sum + arr[i];
+	}
+	
+	printf("\nThe array is : ");
+	
+	for(i=0; i<n; i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	
+	printf("\n\nThe avarage of all elements of array is : %.2f",sum/n);
+	
+	return 0;
+}
