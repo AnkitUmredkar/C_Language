c615fa416fd9c675fe6a13cb39cb3a54db48aef3
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-07 15:37:11 +0530

    Add files via upload

---
+++ 1D_Array/Task 2/decending_sequence_1Darray.c
@@ -0,0 +1,43 @@
+//6 wap to acending order in 1d array.
+
+#include<stdio.h>
+
+int main()
+{
+	int i,j,n;
+	
+	printf("Enter the size of array : ");
+	scanf("%d",&n);
+	
+	int a[n],temp;
+	
+	printf("\nEnter the elements of array \n");
+	
+	for(i=0; i<n; i++)
+	{
+		printf("Enter the value of a[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
+	
+	for(i=0; i<n; i++)
+	{
+		for(j=i+1; j<n; j++)
+		{
+			if(a[i] < a[j])
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	
+	printf("\nThe acending sequence of array is : ");
+	
+	for(i=0; i<n; i++)
+	{
+		printf("%d ",a[i]);
+	}
+	
+ 	return 0;
+}
