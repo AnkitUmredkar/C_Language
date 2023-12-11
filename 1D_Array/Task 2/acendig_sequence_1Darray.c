d1f295eba58e0a7d1ffdfb00d941eb9659959ad9
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-11 11:15:44 +0530

    Update acendig_sequence_1Darray.c

--- 1D_Array/Task 2/acendig_sequence_1Darray.c
+++ 1D_Array/Task 2/acendig_sequence_1Darray.c
@@ -27,8 +27,26 @@ int main()
 		{
 			printf("%d ",arr[i]);
 		}
-		
+	}
+	for(i=0; i<n; i++)
+	{
+		for(j=i+1; j<n; j++)
+		{
+			if(a[i] < a[j])
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
 	}
 	
+	printf("\nThe decending sequence of array is : ");
 	
+	for(i=0; i<n; i++)
+	{
+		printf("%d ",a[i]);
+	}
+	return 0:
+}
 }
