d2e51df80e4aeb4dfe8a5e06b50c314e42dfb5dd
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-12 21:02:50 +0530

    Add files via upload

---
+++ String/reverse_of_string.c
@@ -0,0 +1,26 @@
+//4. WAP to given string in reverse.
+
+#include<stdio.h>
+
+int main()
+{
+	char a[100];
+	int len = 0,i;
+	
+	printf("Enter The string you want : ");
+	scanf("%s",&a);
+	
+	while(a[len] != '\0')
+	{
+		len++;
+	}
+	
+	printf("\nThe reverse of string is : ");
+	
+	for(i=len-1; i>=0; i--)
+	{
+		printf("%c",a[i]);
+	}
+	
+	return 0;
+}
