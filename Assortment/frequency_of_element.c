a2eb44cc38ae9209f1fa5bf1f68ae1b2f8aceaec
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-14 15:46:44 +0530

    Update frequency_of_element.c

--- Assortment/frequency_of_element.c
+++ Assortment/frequency_of_element.c
@@ -4,47 +4,46 @@
 
 int main()
 {
-	int l,sum,num;
+	int i,j,freq=1,n;
 	
-	printf("Enter The Value Of Size 1D Array :");
-	scanf("%d",&l);
+	printf("Enter the size of array : ");
+	scanf("%d",&n);
 	
-	int a[l],b[l+50];
+	int a[n];
 	
-	int i,j,check=0,count;
+	printf("\nEnter the elemnets of an array : \n");
 	
-	for(i=0; i<l; i++)
+	for(i=0; i<n; i++)
 	{
-		printf("Enter The Value Of Array : ");
+		printf("Enter the value of a[%d] : ",i);
 		scanf("%d",&a[i]);
-		b[i]=-1;
 	}
-	int k;
-	for(i=0; i<l; i++)
+	
+	printf("\nThe array is : ");
+	
+	for(i=0; i<n; i++)
 	{
-		count=1;
-		for(k=i+1; k<l; k++)
+		printf("%d ",a[i]);
+	}
+	
+	printf("\n\nThe frequency of all elements is : \n");
+	
+	for(i=0; i<n; i++)
+	{
+		for(j=i+1; j<n; j++)
 		{
-			if(a[i]==a[k])
+			if(a[i] == a[j])
 			{
-				count++;
-				b[k]=0;
+				freq++;
+				a[j] = 0;
 			}
 		}
-		if(b[i]!=0)
+		if(a[i] != 0)
 		{
-			b[i]=count;
+			printf("%d --> %d\n",a[i],freq);
 		}
+		freq=1;
 	}
 	
-	printf("\n");
-	
-	for(i=0; i<l; i++)
-	{
-		if(b[i]!=0)
-		{
-			printf("%d --> %d\n",a[i],b[i]);
-		}
-	}
-        return 0;
+	return 0;
 }
