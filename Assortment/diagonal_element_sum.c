65c8526a01bfc138e46ea4a5ff559da1805713ca
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-15 12:05:47 +0530

    Update diagonal_element_sum.c

--- Assortment/diagonal_element_sum.c
+++ Assortment/diagonal_element_sum.c
@@ -1,44 +1,53 @@
 //9. Wap to find sum of diagonal element in 2d array.
 
-#include <stdio.h>
+#include<stdio.h>
 
-int main() 
+int main()
 {
-    int n,i,j;
-
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
-
-    int arr[n];
-
-    printf("Enter the elements of the array:\n");
-    for (int i = 0; i < n; i++)
-	{
-        scanf("%d", &arr[i]);
-    }
-
-    printf("Element  Frequency\n");
-
-    for (int i = 0; i < n; i++)
-	 {
-        if (arr[i] != -1) 
-		{
-            int count = 1;
-
-            for (int j = i + 1; j < n; j++)
-			 {
-                if (arr[i] == arr[j]) 
-				{
-                    count++;
-                    arr[j] = -1;  
-                }
-            }
-
-            printf("%d %d\n", arr[i], count);
-        }
-    }
-
-    return 0;
-}
-
+	int rows,cols,i,j,sum=0;
 	
+	printf("Enter the number of rows : ");
+	scanf("%d",&rows);
+	
+	printf("Enter the number of cols : ");
+	scanf("%d",&cols);
+	
+	int arr[rows][cols];
+	
+	printf("\nEnter the elements of 2D array :\n");
+	
+	for(i=0; i<rows; i++)
+	{
+		for(j=0; j<cols; j++)
+		{
+			printf("Enter the value of arr[%d][%d] : ",i,j);
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	
+	printf("\n2D array is :\n");
+	
+	for(i=0; i<rows; i++)
+	{
+		for(j=0; j<cols; j++)
+		{
+			printf("%d  ",arr[i][j]);
+		}
+		printf("\n");
+	}
+	
+	for(i=0; i<rows; i++)
+	{
+		for(j=0; j<cols; j++)
+		{
+			if(i == j)
+			{
+				sum = sum + arr[i][j];
+			}
+		}
+	}
+	
+	printf("\nThe sum of all anti-diagonal elements is : %d",sum);
+	
+	return 0;
+}
