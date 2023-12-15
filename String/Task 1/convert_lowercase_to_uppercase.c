3fdd8e4eefed8b0b397c5eccabbbc053cf11c5f1
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-15 08:18:13 +0530

    Update convert_lowercase_to_uppercase.c

--- String/Task 1/convert_lowercase_to_uppercase.c
+++ String/Task 1/convert_lowercase_to_uppercase.c
@@ -1,24 +1,27 @@
-//2. WAP to convert given string in uppercase.
-
+//1. WAP to convert given string in uppercase.
+	
 #include<stdio.h>
-
+#include<string.h>
+	
 int main()
 {
 	char a[100];
-	int len = 0,i;
-	
-	printf("Enter The string you want in lowercase : ");
+	int len,i;
+		
+	printf("Enter The string you want : ");
 	gets(a);
-	
-	for(i=0; i<100; i++)
+		
+	len = strlen(a);
+		
+	for(i=0; i<len; i++)
 	{
-		if(a[i]>='a' && a[i]<='z' )
+		if(a[i]>='a' && a[i]<='z')
 		{
 			a[i] = a[i] - 32;
 		}
 	}
-	
+		
 	printf("\nThe uppercase of entered string is : %s",a);
-	
+		
 	return 0;
 }
