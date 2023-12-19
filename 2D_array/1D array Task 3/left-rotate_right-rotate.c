9d9dd27e22f41a386ef81f186eccfed9feac7eb0
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-19 10:47:03 +0530

    Add files via upload

---
+++ 2D_array/1D array Task 3/left-rotate_right-rotate.c
@@ -0,0 +1,26 @@
+// 4. Write a C program to left-rotate and right-rotate an array without using another array.
+
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int l;
+	printf("Enter The Value Of Size 1D Array :");
+	scanf("%d",&l);
+	int i,j,s=0;
+	int a[l];
+	for(i=0; i<l; i++)
+	{
+		printf("Enter The Value Array[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
+	printf("\n");
+	int n,o=l;
+	for(i=0; i<=l-1; i++)
+	{
+		o--;
+		s=a[n];
+		printf("%d ",s);
+	}
+}
