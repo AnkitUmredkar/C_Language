05b9f3750af9dcd4cdb45e2cf9327ea175632a46
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-23 15:51:08 +0530

    Update remove_space_from_string.c

--- String/Task 2/remove_space_from_string.c
+++ String/Task 2/remove_space_from_string.c
@@ -5,34 +5,32 @@
 
 int main()
 {
-	char a[50];
-	int i,j,len,check=0;
-	
-	printf("Enter the string : ");
-	gets(a);
-		
-	len = strlen(a);
+	int len,i,j;
+ 	char a[50];
+ 	
+ 	printf("Enter the string : ");
+ 	gets(a);
+ 	
+ 	len = strlen(a);
+ 	
+ 	for(i=0; i<len; i++)
+ 	{
+ 		if(a[i]==32)
+		{
+			for(j=i+1; j<len; j++)
+			{
+				a[j-1] = a[j];
+			}
+			i--;
+			len--;
+		}
+	}
 	
 	for(i=0; i<len; i++)
 	{
-		for(j=0; j<len; j++)
-		{
-			if(a[j] == ' ')
-			{
-				check = 1;
-			}
-			if(check == 1)
-			{
-				a[j] = a[j+1];
-			}
-		}
-		check = 0;
+		printf("%c",a[i]);
 	}
-		
-	printf("\nAfter remove all spaces in string : ");
-	printf("%s",a);
-
 	
- 	return 0;
+	return 0;
 }
 
