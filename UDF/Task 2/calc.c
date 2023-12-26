859cfc8ae56b289b48af8e73461c76bdfc9d7bc4
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-26 17:01:06 +0530

    Update calc.c

--- UDF/Task 2/calc.c
+++ UDF/Task 2/calc.c
@@ -20,55 +20,56 @@ void division(float a,float b)
 	printf("%0.2f / %0.2f = %0.2f",a,b,a/b);
 }
 
-float calculator()
-{
-	char op;
- 	float a,b;
- 	st:
- 		if(op!='0')
- 		{
- 			
-	while(1)
-	{
-	printf("\n\nEnter the operator(+,-,*,/) : ");
-	scanf(" %c",&op);
-	 	if(op=='0')
-	 	{
-	 		goto st;
-		}
-	printf("Enter the first value a : ");
-	scanf("%f",&a);
-	 	
-	printf("Enter the first value b : ");
-	scanf("%f",&b);
-	 	
-	switch(op)
- 	{
-	 	case '+' : sum(a,b);
-			break;
-			
-	 	case '-' : substraction(a,b);
-			break;
-			
-		case '*' : multiplication(a,b);
-			break;
-			
-		case '/' : division(a,b);
-			break;
-			
-		default : printf("You Enter unvalid operator, so it can't process"); 
-				
-		}
-	}
-	}
-		 }
 	
-
-
-
+	
 int main()
 {
- 		calculator();
+ 	char op;
+ 	float a,b;
  	
- 	return 0;
+ 	printf("If you want to stop calc then press : 0, in operator input\n");
+ 	
+ 	st:
+ 	if(op!='0')
+ 	{
+ 			
+		while(1)
+		{
+			printf("\n\nEnter the operator(+,-,*,/) : ");
+			scanf(" %c",&op);
+			
+			if(op=='0')
+			{
+			 	goto st;
+			}
+			
+			printf("Enter the first value a : ");
+			scanf("%f",&a);
+			 	
+			printf("Enter the first value b : ");
+			scanf("%f",&b);
+			 	
+			switch(op)
+		 	{
+			 	case '+' : sum(a,b);
+					break;
+					
+			 	case '-' : substraction(a,b);
+					break;
+					
+				case '*' : multiplication(a,b);
+					break;
+					
+				case '/' : division(a,b);
+					break;
+					
+				default : printf("You Enter unvalid operator, so it can't process"); 
+						
+			}
+		}
+	}
+	
+	return 0;
 }
+ 	
+ 
