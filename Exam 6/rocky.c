bbd02fec0edde0bc3f6c81e52e9ea22f7ff155d6
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-28 15:33:59 +0530

    Add files via upload

---
+++ Exam 6/rocky.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int part1()
+{
+	int a=30;
+	return a;
+}
+int part2()
+{
+	int b=30;
+	return b;
+}
+int part3()
+{
+	int c=30;
+	return c;
+}
+
+int dealer(int a,int b,int c)
+{
+	int d = a+b+c;
+	return d;
+}
+int rocky()
+{
+	int e=10+dealer(part1(), part2() , part3());
+	return e;
+}
+
+int main()
+{
+//	int final,n=10;
+//	
+//	final = n + part3();
+	
+	printf("%d",rocky());
+}
