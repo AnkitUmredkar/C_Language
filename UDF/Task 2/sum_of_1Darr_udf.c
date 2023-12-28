9842b934c3f3beb5db3965c267ce922f83830f6b
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-28 10:03:58 +0530

    Update sum_of_1Darr_udf.c

--- UDF/Task 2/sum_of_1Darr_udf.c
+++ UDF/Task 2/sum_of_1Darr_udf.c
@@ -1,37 +1,50 @@
 //1 Write a Program to find the sum of all 1D Array elements by passing an array as an argument using UDF.
 
+//2 Write a Program to find the sum of all 1D Array elements by passing an array as an argument using UDF
+
 #include<stdio.h>
 
-int sumofarray(int a[],int n)
+int sum(int a[],int n)
 {
-	int sum = 0,i;
-	
-	printf("\nEnter the elements of an array : \n");
- 	
- 	for(i=0; i<n; i++)
- 	{
- 		printf("Enter the a[%d] : ",i);
-		scanf("%d",&a[i]);	
-	}
+	int sum=0,i;
 	
 	for(i=0; i<n; i++)
- 	{
-		sum = sum + a[i];	
+	{
+		sum = sum + a[i];
 	}
 	
-	return sum;
+	return sum;	
+}
+
+int userinput()
+{
+	int i,n;
+	
+	printf("Enter the size of an array : ");
+	scanf("%d",&n);
+	int a[n];
+	
+	printf("\nEnter the elements of an array :\n");
+	
+	for(i=0; i<n; i++)
+	{
+		printf("Enter the a[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
+	
+	printf("\nThe array is : ");
+	
+	for(i=0; i<n; i++)
+	{
+		printf("%d ",a[i]);
+	}
+	
+	return sum(a,n);
 }
 
 int main()
-{
- 	int n,i;
- 	
- 	printf("Enter the size of an array : ");
- 	scanf("%d",&n);
- 	
- 	int a[n];
- 	
- 	printf("\nThe sum of an array is : %d",sumofarray(a,n));
- 	
+{	
+	printf("\n\nThe sum of all elements of an array is : %d",userinput());
+	
  	return 0;
 }
