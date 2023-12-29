dfdc4f92df995a7c32ac584270b30730135ec4cf
AnkitUmredkar <149374001+AnkitUmredkar@users.noreply.github.com>
2023-12-29 15:26:48 +0530

    Add files via upload

---
+++ Pointer/Task 1/qu_1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+ 	int n,i;
+ 	
+ 	printf("Enter the size of an array  : ");
+ 	scanf("%d",&n);
+ 	
+ 	int a[n];
+ 	
+ 	printf("\nEnter the elements : \n");
+ 	
+ 	for(i=0; i<n; i++)
+ 	{
+ 		printf("Enter the a[%d] : ",i);
+ 		scanf("%d",&a[i]);
+	}
+	
+	printf("\nThe reverse array is : ");
+	
+	int *ptr = &a[n-1];
+	
+	for(i=n-1; i>=0; i--)
+ 	{
+ 		printf("%d ",*ptr);
+ 		ptr-=1;
+	}
+ 	
+ 	return 0;
+}
